73bff06dbebec8a0cce5ac97d38c6301c47ddc64
leobuck <leobuck12@gmail.com>
2022-12-05 21:25:10 -0300

    Double

--- projeto_inicial/bin/projeto_inicial.dart
+++ projeto_inicial/bin/projeto_inicial.dart
@@ -1,6 +1,10 @@
 void main() {
   int idade = 24;
   int hexadecimal = 0x000018;
+  double altura = 1.86;
+  double exponencial = 780e6;
   print(idade);
   print(hexadecimal);
+  print(altura);
+  print(exponencial);
 }
