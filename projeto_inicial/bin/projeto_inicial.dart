d7d0c04ad81dfca9e063336035462a26fc3eaf7c
leobuck <leobuck12@gmail.com>
2022-12-05 21:40:40 -0300

    Booleans

--- projeto_inicial/bin/projeto_inicial.dart
+++ projeto_inicial/bin/projeto_inicial.dart
@@ -3,8 +3,12 @@ void main() {
   int hexadecimal = 0x000018;
   double altura = 1.86;
   double exponencial = 780e6;
+  bool geek = true;
+  bool comparacao = idade == altura;
   print(idade);
   print(hexadecimal);
   print(altura);
   print(exponencial);
+  print(geek);
+  print(comparacao);
 }
