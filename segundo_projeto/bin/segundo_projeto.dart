aeef4a0843eaf3eb40968698e329ebe09c920541
leobuck <leobuck12@gmail.com>
2023-01-23 21:04:43 -0300

    O que é Herança?

--- segundo_projeto/bin/segundo_projeto.dart
+++ segundo_projeto/bin/segundo_projeto.dart
@@ -43,6 +43,47 @@ class Fruta {
   }
 }
 
+class Alimento {
+  String nome;
+  double peso;
+  String cor;
+
+  Alimento(this.nome, this.peso, this.cor);
+}
+
+class Legumes {
+  String nome;
+  double peso;
+  String cor;
+  bool isPrecisaCozinhar;
+
+  Legumes(this.nome, this.peso, this.cor, this.isPrecisaCozinhar);
+}
+
+class Citricas {
+  String nome;
+  double peso;
+  String cor;
+  int diaDesdeColheita;
+  bool? isMadura;
+  double nivelAzedo;
+
+  Citricas(
+      this.nome, this.peso, this.cor, this.diaDesdeColheita, this.nivelAzedo);
+}
+
+class Nozes {
+  String nome;
+  double peso;
+  String cor;
+  int diaDesdeColheita;
+  bool? isMadura;
+  double porcentagemOleoNatural;
+
+  Nozes(this.nome, this.peso, this.cor, this.diaDesdeColheita,
+      this.porcentagemOleoNatural);
+}
+
 // Função void
 // void estaMadura() {}
 
