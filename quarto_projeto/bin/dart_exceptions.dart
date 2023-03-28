02e200936cdae7f6277a508e37115155fd04eff9
leobuck <leobuck12@gmail.com>
2023-03-27 21:41:38 -0300

    Erros x Exceções

--- quarto_projeto/bin/dart_exceptions.dart
+++ quarto_projeto/bin/dart_exceptions.dart
@@ -1,25 +1,35 @@
 import 'controllers/bank_controller.dart';
 import 'models/account.dart';
 
+// void main() {
+//   // Criando o banco
+//   BankController bankController = BankController();
+
+//   // Adicionando contas
+//   bankController.addAccount(
+//       id: "Ricarth",
+//       account:
+//           Account(name: "Ricarth Lima", balance: 400, isAuthenticated: true));
+
+//   bankController.addAccount(
+//       id: "Kako",
+//       account:
+//           Account(name: "Caio Couto", balance: 600, isAuthenticated: true));
+
+//   // Fazendo transferência
+//   bool result = bankController.makeTransfer(
+//       idSender: "Kako", idReceiver: "Ricarth", amount: 700);
+
+//   // Observando resultado
+//   print(result);
+// }
+
 void main() {
-  // Criando o banco
-  BankController bankController = BankController();
+  // double amount = double.parse("25,33");
+  recursiveFunc(1);
+}
 
-  // Adicionando contas
-  bankController.addAccount(
-      id: "Ricarth",
-      account:
-          Account(name: "Ricarth Lima", balance: 400, isAuthenticated: true));
-
-  bankController.addAccount(
-      id: "Kako",
-      account:
-          Account(name: "Caio Couto", balance: 600, isAuthenticated: true));
-
-  // Fazendo transferência
-  bool result = bankController.makeTransfer(
-      idSender: "Kako", idReceiver: "Ricarth", amount: 700);
-
-  // Observando resultado
-  print(result);
+recursiveFunc(int count) {
+  print(count);
+  recursiveFunc(count + 1);
 }
