e9ca0ab3877da3b6d1cc5bc88293d4e2c38b9c29
leobuck <leobuck12@gmail.com>
2024-03-30 16:03:14 -0300

    Aplicando o Future Delayed.

--- quinto_projeto/bin/future.dart
+++ quinto_projeto/bin/future.dart
@@ -4,5 +4,13 @@ void main() {
     return 21;
   }).then((value) => print('The value is $value'));
 
+  Future<int> myFutureFunc() async {
+    print('I have a function in the Future');
+    await Future.delayed(Duration(seconds: 5));
+    return 12;
+  }
+
+  myFutureFunc().then((value) => print('My function value is: $value'));
+
   print('Done with main');
 }
