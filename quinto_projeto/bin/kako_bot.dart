7218c1cd8d44239d3a7f42ab07bba5be2eec26d5
leobuck <leobuck12@gmail.com>
2024-03-30 17:26:20 -0300

    Implementando o tempo de resposta

--- quinto_projeto/bin/kako_bot.dart
+++ quinto_projeto/bin/kako_bot.dart
@@ -1,7 +1,7 @@
-
 import 'dart:io';
 
 import 'questions/time_questions.dart';
+import 'timing/waiting_time.dart';
 
 void main() async {
   String kakoBot = 'KakoBOT:\n';
@@ -9,11 +9,13 @@ void main() async {
   String usuario = '';
 
   print('-- Iniciando o KakoBOT, aguarde..--');
+  await BotClock().clock(2);
 
   print('KakoBOT:\n Oi :) \n Como posso ajudar?');
   do {
     usuario = stdin.readLineSync().toString();
     print('-- Processando pergunta, aguarde..--');
+    await BotClock().clock(1);
     if (usuario.contains('xau') ||
         usuario.contains('Xau') ||
         usuario.contains('Adeus') ||
@@ -22,10 +24,12 @@ void main() async {
       print(kakoBot + ' Até a proxima!!');
     } else if (TimeQuestions(usuario).isThisTime()) {
       // verificar antes, assim não fazemos toda a função sem precisar.
+      await BotClock().clock(2);
       TimeQuestions(usuario).timeQuestion();
     } else if (false) {
       //Basta adicionar novas perguntas aqui!
     } else {
+      await BotClock().clock(2);
       print(kakoBot +
           ' Não fui treinado para responder a essa pergunta \n Desculpe :( ');
       print(kakoBot + ' Você pode fazer outra pergunta ou dizer Adeus');
